f34cdb7aefaff6d337404eb9479f5defb567ca67
Pranithakakarlamudi9 <parnithakakarlamudi@gmail.com>
2020-08-06 20:00:32 +1000

    Add project files.

---
+++ project/Test.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+
+namespace project
+{
+    [TestFixture]
+    class Test:Website
+    {
+
+        [Test,Order(1)]
+        //test travel method
+        public void TestTravel()
+        {
+            Website web = new Website();
+            
+            web.LuanchBrowser();
+            web.NavigateToTravel();
+
+            //check wheteher sub topics "Tell me more"/"Discover more"
+            Thread.Sleep(5000);
+            IWebElement _discoverMore = driver.FindElement(By.LinkText("Discover more"));
+            String _discoverMoreText= driver.FindElement(By.LinkText("Discover more")).Text;
+            String _expected ="Discove more";
+
+            if (_discoverMore.Displayed)
+            {
+                Assert.Equals(_expected,_discoverMoreText);
+            }
+
+        }
+        //tets log on by asserting presence of textboxes username and password
+        [Test,Order(2)]
+        public void TestLogOn()
+        {
+            Website web = new Website();
+
+            web.LuanchBrowser();
+            web.NavigateToTravel();
+            web.LogOn();
+            driver.SwitchTo().ActiveElement();
+            driver.FindElement(By.Id("txtMyClientNumber_field")).Click();
+            IWebElement _username = driver.FindElement(By.Id("txtMyClientNumber_field"));
+            driver.FindElement(By.Id("txtMyPassword_field")).Click();
+            IWebElement _password = driver.FindElement(By.Id("txtMyPassword_field"));
+
+            if (_username.Displayed && _password.Displayed)
+            {
+                Assert.Pass();
+            }
+
+
+
+
+
+
+        }
+
+    }
+}
---
+++ project/Website.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+
+namespace project
+{
+    class Website
+    {
+        public static IWebDriver driver;
+        public void LuanchBrowser()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            //launching browser nad navigate to website
+            driver.Navigate().GoToUrl("https://www.commbank.com.au/");
+
+        }
+        public void NavigateToTravel()
+        {
+            //click on Travel
+            driver.FindElement(By.XPath("//*[@id='products']/div/div/div[1]/div[8]/div/a/div[2]/div")).Click();
+            var element = driver.FindElement(By.XPath("//*[@id='products']/div/div/div[1]/div[8]/div/a/div[2]/div")) ;
+            Actions actions = new Actions(driver);
+            actions.MoveToElement(element);
+            actions.Perform();
+            //click on Tell me more/discover more
+            driver.FindElement(By.LinkText("Discover more"));
+            
+
+
+        }
+        public void LogOn()
+        {
+            //click on logon
+            driver.FindElement(By.XPath("//*[@id='gloNavUnauth']/nav[1]/div/div/ul[2]/li[2]/a[1]")).Click();
+        }
+        
+
+    }
+}
